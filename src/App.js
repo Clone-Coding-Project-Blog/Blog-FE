4143f9cbc0763df437fce09d51928148228524a1
Jin Oh <pinion0801@gmail.com>
2022-05-02 16:56:28 +0900

    :sparkles: Login 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+//
 import Home from './pages/Home';
+import Login from './pages/Login';
+
+//
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
     return (
-      <Router>
-        <Routes>
-          <Route element={<Home />} path="/" />
-        </Routes>
+        <Router>
+            <Header />
+            <Routes>
+                <Route element={<Home />} path="/" />
+                <Route element={<Login />} path="/user/login" />
+            </Routes>
         <Footer />
-      </Router>
+</Router>
     );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import tw, { styled } from 'twin.macro';
+
+function Login() {
+    return(
+        <div></div>
+    );
+}
+
+export default Login;
